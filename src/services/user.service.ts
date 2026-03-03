d4b9475f52d7134a1c6b5d6976ddaa1b86909a76
sagarhasan2024 <sagar.hasan@lyxa.ai>
2026-03-04 05:03:02 +0600

    recent room works

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -78,15 +78,13 @@ export class UserService {
 
       const user = await this.repository.getUser(userId);
 
-      console.log(user)
-
       const { password, ...userWithoutPassword } = user;
       return userWithoutPassword;
     } catch (error) {
       if (error instanceof AppError) {
         throw error;
       }
-      console.log(error);
+
       throw new AppError('Failed to get user by ID!', 500, 'User Service');
     }
   }
